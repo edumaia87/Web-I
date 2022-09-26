d57097da724206eeac9a1afd12f027fa4c85806c
Eduardo Maia <edumaia89@outlook.com>
2022-09-26 16:57:25 -0300

    introdução ao css

---
+++ Introdução ao CSS/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introdução ao CSS</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="content">
+        <h1>Sistema para Veículos</h1>
+
+        <p id="p1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente quisquam minus quia reprehenderit beatae, odit error enim, fugit incidunt amet voluptatum. Odit minus dolor dignissimos, nesciunt eius tenetur ea! Officiis?</p>
+    </div>
+
+    <button class="botaoTipo1">Botão 1</button>
+    <button class="botaoTipo1">Botão 2</button>
+    <button class="botaoTipo1">Botão 3</button>
+    <button class="botaoTipo1">Botão 4</button>
+</body>
+</html>
