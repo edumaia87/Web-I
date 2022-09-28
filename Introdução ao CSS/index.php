5f5ed0dd154a43f4811e7d122115686dc358cd59
Eduardo Maia <92751711+edumaia87@users.noreply.github.com>
2022-09-28 11:00:58 -0300

    atividade 7

--- Introdução ao CSS/index.php
+++ Introdução ao CSS/index.php
@@ -8,15 +8,75 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="content">
-        <h1>Sistema para Veículos</h1>
-
-        <p id="p1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente quisquam minus quia reprehenderit beatae, odit error enim, fugit incidunt amet voluptatum. Odit minus dolor dignissimos, nesciunt eius tenetur ea! Officiis?</p>
+    <nav class="header">
+        <ul class="links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#paragraph">Parágrafo</a></li>
+        </ul>
+    </nav>
+    
+    <div id="home" class="grid">
+        <div class="col">
+            <div class="col-content">
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-content">      
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-content">      
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-content">      
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-content">      
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-content">      
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-content">
+      
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-content">
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-content">  
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-content">   
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-content">
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-content">
+            </div>
+        </div>
     </div>
 
-    <button class="botaoTipo1">Botão 1</button>
-    <button class="botaoTipo1">Botão 2</button>
-    <button class="botaoTipo1">Botão 3</button>
-    <button class="botaoTipo1">Botão 4</button>
+    <div class="buttons">
+        <button class="botaoTipo1">Botão 1</button>
+        <button class="botaoTipo1">Botão 2</button>
+        <button class="botaoTipo1">Botão 3</button>
+        <button class="botaoTipo1">Botão 4</button>
+    </div>
+
+    <div id="paragraph" class="paragraph">
+        <h1>Lorem Ipsum</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati doloremque cupiditate, optio sit, illo asperiores ullam quia quae quis amet, corrupti provident nesciunt iure dicta autem exercitationem nam animi est?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati doloremque cupiditate, optio sit, illo asperiores ullam quia quae quis amet, corrupti provident nesciunt iure dicta autem exercitationem nam animi est?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati doloremque cupiditate, optio sit, illo asperiores ullam quia quae quis amet, corrupti provident nesciunt iure dicta autem exercitationem nam animi est?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati doloremque cupiditate, optio sit, illo asperiores ullam quia quae quis amet, corrupti provident nesciunt iure dicta autem exercitationem nam animi est?</p>
+    </div>
 </body>
 </html>
